e6c05338dfe90510021dc2aa715de984474bde1f
altiarindikaa <altiarindikaa@students.amikom.ac.id>
2023-04-10 15:30:11 +0700

    tugas

---
+++ Dosen.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dosen
+{
+     public Dosen
+    {
+        private int ID = 1;
+        private string Nama = Dosen pengampu; 
+        private int NIK = 123456789;
+        private string Gender = male;
+        private string Course = Pemrograman Berorientasi Objek;
+
+        private Dosen(int id, string nama, int nik, string gender, string course)
+        {
+            this.ID = 1;
+            this.Nama = Dosen Pengampu;
+            this.NIK = 123456789;
+            this.Gender = Male;
+            this.Course = Pemograman Berorientasi Objek;
+        }
+
+        public void
+
+    }
+}
